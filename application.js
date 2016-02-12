4536c09a444101c306f006c7346539038636695e
Nick Yee <njsyee@gmail.com>
2016-02-12 16:18:26 -0700

    Got map to load

--- application.js
+++ application.js
@@ -1,1 +1,35 @@
-console.log(config);
+$(function() {
+  var map;
+  var marker;
+  var myLatLng = {
+    lat: -34.397,
+    lng: 150.644
+  }
+
+  initializeAPI();
+
+  initializeMarker();
+
+
+
+
+function initializeMarker(){
+
+}
+
+function initializeAPI() {
+  var body = $("body");
+  body.append("<script async defer src='https://maps.googleapis.com/maps/api/js?key=" +
+    config.mapsAPI + 
+    "&callback=initMap'></script>");
+}
+
+function initMap() {
+  map = new google.maps.Map($("#map"), {
+    center: myLatLng,
+    zoom: 8
+  });
+}
+
+});
+
