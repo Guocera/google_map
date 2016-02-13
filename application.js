82cdfffcb073c9e702df9f97b15166905f3e790d
Nick Yee <njsyee@gmail.com>
2016-02-13 12:03:34 -0700

    Centers on vancouver

--- application.js
+++ application.js
@@ -2,28 +2,29 @@
 $(function() {
   var marker;
   var myLatLng = {
-    lat: -34.397,
-    lng: 150.644
-  }
+    lat: 49.2827,
+    lng: -123.1207
+  };
 
   initializeAPI();
 
-  function initializeMarker(){
+  function initializeMarker(myLatLng){
     marker = new google.maps.Marker({
       position: myLatLng,
       map: map,
       title: "Hello World!"
-    })
+    });
   }
 
   function initializeAPI() {
-    var body = $("body");
     var url = "https://maps.googleapis.com/maps/api/js?key=" +
       config.mapsAPI + 
       "&callback=initMap";
 
     $.getScript(url, function() {
-        initializeMarker();
+        initializeMarker(myLatLng);
+        var vancouver = marker.getPosition();
+        map.setCenter(vancouver);
     });
     
   }
